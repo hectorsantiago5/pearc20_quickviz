ac8088531813d0083969f695a4120642fa244394
Hector Santiago <56176507+hectorsantiago5@users.noreply.github.com>
2020-07-27 22:59:02 -0400

    * Initial commit    - Created a simple Flask app    - Wrote functions to handle getting covid data and user file upload    - Created an uploads folder to handle all user uploads    - Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pearc20_quickviz
+
+This project is for the PEARC20 Hackathon and this is the beginnings of a Flask application to provide basic statistical analysis on any dataset uploaded by a user
+
+## Set Up:
+1. Make sure all the dependencies are met. Run pip install -r requirements.txt in your projects terminal
+1. Run app.py
+1. Expose localhost to the internet
+1. If you are on a Mac, you need to install certificates.command to be able to scrape the GitHub website for COVID data.
+    1. Click on Finder
+    1. Click on Applications
+    1. Expand the Python folder
+        1. Double click on the install certificates.command terminal script
---
+++ app.py
@@ -0,0 +1,117 @@
+
+import csv
+import os
+# from collections import OrderedDict
+# from csv import DictReader
+# from typing import Dict, Union
+from urllib.request import urlretrieve as retrieve
+from werkzeug.utils import secure_filename
+# from _collections import defaultdict
+
+from flask import Flask, render_template, request, redirect, flash, url_for
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# Function to connect our HTML file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('aboutus.html')
+
+
+# Function to get a COVID-19 data file and display the contents of it
+@app.route('/covid')
+def import_covid_csv():
+    url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+    retrieve(url, 'covid_file/us.csv')
+
+    with open('covid_file/us.csv', 'r') as covidfile:
+        csv_read = csv.DictReader(covidfile)
+        #print(csv_read)
+        covid_list = []
+
+        # For loop to get the last 7 days of Covid Data
+        for j in list(reversed(list(csv_read)))[0:7]:
+            #print(j)
+            date = j['date']
+            cases = j['cases']
+            deaths = j['deaths']
+            covid_list.append({'date': date, 'cases': cases, 'deaths': deaths})
+
+        # Function to compute and display the COVID-19 death rate
+        def compute_csv():
+            data = []
+            with open('covid_file/us.csv', 'r') as covidfile:
+                csv_read = csv.DictReader(covidfile)
+                for i in list(reversed(list(csv_read))):
+                    # print(i)
+                    cases = i['cases']
+                    deaths = i['deaths']
+                    rate = int(deaths) / int(cases)
+                    data.append({'cases': cases, 'deaths': deaths, 'rate': rate})
+                    return data
+
+        return render_template('covid.html', l=covid_list, k=compute_csv())
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('data.html')
+
+
+# Function to get a data file and display the contents of it
+@app.route('/uploader', methods=['GET', 'POST'])
+def get_second_csv():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also submits an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Successful')
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+
+    def compute_second_csv():
+        pass
+    #     csv_data = []
+    #     with open('uploads/', filename) as second_file:
+    #         for i in list(reversed(list(second_file))):
+    #             # print(i)
+    #             cases = i['cases']
+    #             deaths = i['deaths']
+    #             rate = int(deaths) / int(cases)
+    #             csv_data.append({'cases': cases, 'deaths': deaths, 'rate': rate})
+    #             print(csv_data)
+    #
+    # compute_second_csv()
+    #
+    # return render_template('data.html')
+
+
+app.secret_key = 'some_secret_key'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
